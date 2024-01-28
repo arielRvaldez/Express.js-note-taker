071bdc5a80816f7f34eafebe8bfb2ae4146b0530
arielRvaldez <148600743+arielRvaldez@users.noreply.github.com>
2024-01-27 19:48:41 -0600

    added api route

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const path = require('path');
-// const { clog } = require('./middleware/clog');
+const { clog } = require('./middleware/clog');
 const api = require('./public/assets/js/index.js');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// app.use(clog);
+app.use('/api', api);
+
+app.use(clog);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
 
 app.use(express.static('public'));
 
