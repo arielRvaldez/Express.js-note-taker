7bc333b0e6792b668fa861aca08ff5003e1a4a4f
arielRvaldez <148600743+arielRvaldez@users.noreply.github.com>
2024-01-27 19:38:34 -0600

    debugging

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -193,7 +193,7 @@ const renderNoteList = async (notes) => {
 // Gets notes from the db and renders them to the sidebar
 const getAndRenderNotes = () => getNotes().then(renderNoteList);
 
-if (window.location.pathname === '/notes') {
+if (typeof window !== 'undefined' && window.location.pathname === '/notes') {
   saveNoteBtn.addEventListener('click', handleNoteSave);
   newNoteBtn.addEventListener('click', handleNewNoteView);
   clearBtn.addEventListener('click', renderActiveNote);
