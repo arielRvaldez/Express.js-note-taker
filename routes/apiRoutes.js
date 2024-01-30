82d6a4aea4d461155aebf2fd75000edbf3ab13f1
arielRvaldez <148600743+arielRvaldez@users.noreply.github.com>
2024-01-29 20:19:14 -0600

    changes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,33 +7,56 @@ const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 // readFromFile('./db/db.json').then((data) => {
 // localhost:3001/api/notes 
 router.get("/notes", (req, res) => {
-  readFromFile('./db/db.json').then((data) => 
-
-    res.json(JSON.parse(data)));
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
 router.post("/notes", (req, res) => {
-  console.log(req.body);
+  const {title, text} = req.body;
 
-  const isValid = req.body?.isValid;
-  const errors = req.body?.errors;
+  if (title && text) {
+    const newNote = {
+        title,
+        text,
+        note_id: uuidv4(),
+    };
+    readAndAppend(newNote, './db/db.json');
 
-  const payload = {
-    time: Date.now(),
-    error_id: uuidv4(),
-    text: req.body.text,
-    title: req.body.title,
-    errors,
-  };
+    const response = {
+        status: 'success',
+        body: newNote,
+    };
 
-  if (!isValid) {
-    readAndAppend(payload, './db/db.json');
-    res.json('Note added');
-  } else {
-    res.json({
-      message: 'Object is valid, not logging. Check front end implementation',
-      error_id: payload.error_id
-    });
-  }});
+    res.json(response);
+}   else {
+    const response = {
+        status: 'error',
+        message: 'Title and text are required fields.',
+    };
+    //bad status code
+    res.status(400).json(response);
+}
+});
+  // console.log(req.body);
+
+  // const isValid = req.body?.isValid;
+  // const errors = req.body?.errors;
+
+  // const payload = {
+  //   time: Date.now(),
+  //   error_id: uuidv4(),
+  //   text: req.body.text,
+  //   title: req.body.title,
+  //   errors,
+  // };
+
+  // if (!isValid) {
+  //   readAndAppend(payload, './db/db.json');
+  //   res.json('Note added');
+  // } else {
+  //   res.json({
+  //     message: 'Object is valid, not logging. Check front end implementation',
+  //     error_id: payload.error_id
+  //   });
+  // }});
 
 module.exports = router;
