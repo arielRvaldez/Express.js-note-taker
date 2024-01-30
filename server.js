bfa3acfce75ba0e0c342ac8297bdcc2463f3f285
arielRvaldez <148600743+arielRvaldez@users.noreply.github.com>
2024-01-29 22:50:48 -0600

    debug

--- server.js
+++ server.js
@@ -16,14 +16,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
-app.use('/api', apiRoutes);
+// app.use('/api', apiRoutes);
 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'))
 );
 
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
 );
 
 app.listen(PORT, () =>
